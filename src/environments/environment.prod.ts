b835cfedee85655bd66ede44f6f87016c2502523
EthanTuning <tuningfreelance@gmail.com>
2025-08-07 16:57:37 -0700

    new URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
 	production: true,
 	appName: 'MBN Warehouse Mobile App',
-	appBaseUrl: 'https://mbn-warehouse-mobile-app.delightfulglacier-f4e73e43.westus3.azurecontainerapps.io/',
+	appBaseUrl: 'https://mobile.inventory.mikeandbriansnursery.com/',
   azureProfileUrl: 'https://graph.microsoft.com/v1.0/me',
   azureTenantId: 'b0300ad2-8904-4c25-ad06-8596002cf330',
   azureClientId: '27a243c9-b45f-4fc9-b713-3d281a9834d5',

53eea216506c155a488bc42e4a769a1b79d73aca
Julian Welge <julianwelge@gmail.com>
2025-08-16 18:27:18 -0700

    container tracking return receipt report stubbed - sync w mac

---
+++ src/app/modules/container-tracking/components/container-tracking-return-receipt-report/container-tracking-return-receipt-report.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-container-tracking-return-receipt-report',
+  templateUrl: './container-tracking-return-receipt-report.component.html',
+  styleUrls: ['./container-tracking-return-receipt-report.component.scss'],
+})
+export class ContainerTrackingReturnReceiptReportComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
